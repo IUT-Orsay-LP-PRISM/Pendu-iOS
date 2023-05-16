08f7dca7768783ab00be94951e92e20a366f0313
Mathieu MORGAT <morgatmathieu@gmail.com>
2023-05-16 15:27:25 +0200

    Added image and tries functions

--- Pendu-iOS/MenuController.swift
+++ Pendu-iOS/MenuController.swift
@@ -8,14 +8,44 @@
 import UIKit
 
 class MenuController: UIViewController {
-
+    var nbrErreur = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nbrErreur = 0
         // Do any additional setup after loading the view.
     }
     
-
+    @IBOutlet weak var penduImg: UIImageView!
+    @IBOutlet weak var triesLbl: UILabel!
+    @IBOutlet weak var incrementBtn: UIButton!
+    @IBAction func incrementAction(_ sender: Any) {
+        nbrErreur = nbrErreur + 1;
+        changeImg(nbr:nbrErreur);
+        triesLeft(nbr:nbrErreur);
+    }
+    func changeImg(nbr:Int){
+        if(nbr != 10){
+            penduImg.image = UIImage(named: String(nbr+1)+"coup")
+        }
+        else{
+            penduImg.image = UIImage(named: "pendu")
+        }
+    }
+    
+    func triesLeft(nbr:Int){
+        let total = 10;
+        let tries = total - nbr;
+        if(tries > 0){
+            triesLbl.text = "Tentatives restantes : " + String(tries);
+        }
+        else{
+            triesLbl.text = "Aucunes tentatives restantes";
+            incrementBtn.isEnabled = false;
+            
+        }
+        
+        
+    }
     /*
     // MARK: - Navigation
 
