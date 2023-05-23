09210c6909708e5a077dc6386b8b5a014ba2b0b4
Suylo <spoownz@yahoo.com>
2023-05-23 17:27:37 +0200

    feat(pendu): improvements

--- Pendu-iOS/JeuController.swift
+++ Pendu-iOS/JeuController.swift
@@ -11,52 +11,46 @@ class JeuController: UIViewController {
     @IBOutlet weak var penduImg: UIImageView!
     @IBOutlet weak var word: UILabel!
     @IBOutlet weak var textLost: UILabel!
-    var wordChosen: String = "mathieu"
+    
+    var wordChosen: String = "coiffeur"
+    
     var arrayOfWord: [String] = []
     var arrayOfSoluce: [String] = []
     var nbTry: Int = 10
     var counter: Int = 0;
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // On compte le nbre de lettre du mot donné "coiffeur" : 8
-        let wordCount: Int = wordChosen.count
-        // on set le text de word iboutlet text du pendu à rien
-        word.text = ""
         
-        // On parcours le nbre de lettre donc 8 pour afficher 8 _
-        // On divise chaque lettre dans un tableau ["c", "o", "i"]...
         for char in wordChosen{
             arrayOfWord.append(String(char))
             arrayOfSoluce.append("_")
         }
-        updateLabelSoluce()
-        
-        print("Tableau mot solution :")
         print(arrayOfWord)
-        print("Tableau lettre tapée : ")
         print(arrayOfSoluce)
+        updateLabelSoluce()
     }
     
     @IBAction func onClickBtn(_ sender: UIButton) {
         var btnTitle = sender.titleLabel?.text ?? ""
         btnTitle = btnTitle.lowercased()
         var status = false;
+        var ended = false;
+
         
         for (index, element) in arrayOfWord.enumerated(){
             if (element == btnTitle){
                 arrayOfSoluce[index] = btnTitle.uppercased()
                 updateLabelSoluce()
-                sender.isEnabled = false;
+                sender.removeFromSuperview()
                 status = true;
             }
         }
 
         if (!status){
             counter += 1;
-            sender.isEnabled = false;
-                
+            sender.removeFromSuperview()
+
             if (counter >= nbTry){
                 changeImg(nbr:counter);
                 textLost.text = "PERDU !";
@@ -65,7 +59,12 @@ class JeuController: UIViewController {
             }
         }
         
-        
+        checkIfWin()
+    }
+    
+    func checkIfWin(){
+        for element in arrayOfSoluce{
+        }
     }
     
     func updateLabelSoluce(){
