6c5536f26ee72c0bf2e867621901764cc9a3b294
Suylo <spoownz@yahoo.com>
2023-05-23 16:11:19 +0200

    feat(pendu): WIP pendu algo

--- Pendu-iOS/JeuController.swift
+++ Pendu-iOS/JeuController.swift
@@ -8,42 +8,85 @@
 import UIKit
 
 class JeuController: UIViewController {
+    @IBOutlet weak var penduImg: UIImageView!
     @IBOutlet weak var word: UILabel!
+    @IBOutlet weak var textLost: UILabel!
     var wordChosen: String = "coiffeur"
-    var tableOfWord: [String] = []
+    var arrayOfWord: [String] = []
+    var arrayOfSoluce: [String] = []
+    var nbTry: Int = 10
     var counter: Int = 0;
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // On compte le nbre de lettre du mot donné "coiffeur" : 8
         let wordCount: Int = wordChosen.count
+        // on set le text de word iboutlet text du pendu à rien
         word.text = ""
         
-        for _ in 1...wordCount{
-            word.text! += "_"
-        }
+        // On parcours le nbre de lettre donc 8 pour afficher 8 _
+        // On divise chaque lettre dans un tableau ["c", "o", "i"]...
         for char in wordChosen{
-            tableOfWord.append(String(char))
+            arrayOfWord.append(String(char))
+            arrayOfSoluce.append("_")
         }
+        
+        for letter in arrayOfSoluce{
+            word.text! += letter
+        }
+        
+        print("Tableau mot solution :")
+        print(arrayOfWord)
+        print("Tableau lettre tapée : ")
+        print(arrayOfSoluce)
     }
     
     @IBAction func onClickBtn(_ sender: UIButton) {
-        let btnTitle = sender.titleLabel?.text ?? ""
+        // On récupère la valeur du bouton et on la lowercase
+        var btnTitle = sender.titleLabel?.text ?? ""
+        btnTitle = btnTitle.lowercased()
+        var counterArray: Int = 0
         
-        if (tableOfWord[counter] == btnTitle){
-            word.text = tableOfWord[counter] + "_______"
+        // ici que va se jouer la logique :
+        // onClick, on récupère le btnTitle
+        // si le btnTitle est égale à n'importe qu'elle être du mot
+        // ça veut dire que c'est bon du coup le compteur ne bouge pas
+        
+        for (index, element) in arrayOfWord.enumerated(){
+            // On parcours chaque lettre
+            print(String(index) + " - " + element)
+            
         }
-        counter += 1
-    }
-    
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        /*
+         if (letter == btnTitle){
+             print(arrayOfWord[counterArray])
+             counterArray += 1
+         } else {
+             counter += 1;
+             sender.isEnabled = false;
+                 
+             if (counter >= nbTry){
+                 changeImg(nbr:counter);
+                 textLost.text = "PERDU !";
+             } else {
+                 changeImg(nbr:counter);
+             }
+             break;
+         }
+         
+         */
     }
-    */
+    
+    
+    func changeImg(nbr:Int){
+        if(nbr <= 9){
+            penduImg.image = UIImage(named: String(nbr+1)+"coup")
+        }
+        else{
+            penduImg.image = UIImage(named: "pendu")
+        }
+    }
 
 }
