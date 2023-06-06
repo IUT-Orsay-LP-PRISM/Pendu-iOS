0f7fa00ea3ce63201d58484de65acd6f298519e9
Adame Gherbi <agherbi@MACDD05-17.local>
2023-06-06 11:57:51 +0200

    Ajout selector

--- Pendu-iOS/GameController.swift
+++ Pendu-iOS/GameController.swift
@@ -11,7 +11,39 @@ class GameController: UIViewController {
     
     @IBOutlet weak var selectDifficulty: UISegmentedControl!
     @IBOutlet weak var selectTime: UISegmentedControl!
+    @IBOutlet weak var commencer: UIButton!
     
+    
+    @IBAction func commencerr(_ sender: UISegmentedControl) {
+        
+        var selectedDiff = selectDifficulty.selectedSegmentIndex;
+        var selectedTime = selectTime.selectedSegmentIndex;
+        
+        switch selectedDiff{
+                    case 0:
+                    print("Moyen")
+                break;
+                    case 1:
+                    print("Difficile")
+                break;
+            default:
+            print("erreur")
+    }
+        switch selectedTime{
+                    case 0:
+                    print("30 SEC")
+                break;
+                    case 1:
+                    print("90 SEC")
+                break;
+                    case 2:
+                    print("infini")
+                break;
+            default:
+            print("erreur")
+    }
+}
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }
